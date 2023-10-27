b51aa6c7b36910592aaa3db7c803b0e78962f8a8
Joy Ahmed <dev.joyahmed@gmail.com>
2023-10-27 09:48:43 +0600

    updated url endpoint

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('category', CategoryList.as_view(), name="category"),
-    path('product', ProductList.as_view(), name="product"),
+    path('categories', CategoryList.as_view(), name="category"),
+    path('products', ProductList.as_view(), name="product"),
     path('product-image', ProductImageList.as_view(), name="productImage"),
 ]
